4b973de707186486ca8b93b841c3958fce3ec4be
q'w <q'we>
2019-08-25 21:32:43 +0800

    1

--- assets/Script/control.ts
+++ assets/Script/control.ts
@@ -4,20 +4,52 @@ const {ccclass, property} = cc._decorator;
 export default class NewClass extends cc.Component {
     @property(cc.Prefab)
     prefab: cc.Prefab = null
-    @property(cc.Node)
-    item: cc.Node = null
+    @property(Number)
+    speed: number = 1
+	
+    item: cc.Node[] = []
 
     onLoad () {
-        // this.node.removeChild(this.node.getChildByName('begin_item'))
-        this.node.addChild(cc.instantiate(this.prefab))
-        this.node.addChild(cc.instantiate(this.prefab))
-        this.node.addChild(cc.instantiate(this.prefab))
-        this.node.addChild(cc.instantiate(this.prefab))
+        for(let i=0; i<7; i++) {
+            if(this.prefab) {
+                this.item.push(cc.instantiate(this.prefab))
+                this.item[i].y = -870 + 432*i
+                this.node.addChild(this.item[i])
+            }
+        }     
+    }
+
+    click () {
+        console.error('log')
+    }
+
+    moveDown() {
+        return new Promise((resolve) => {
+            for(let i=0; i<7; i++) {
+                let index = i
+                this.item[i].runAction(cc.sequence(cc.moveTo(this.speed,0,this.item[i].y - 432), cc.callFunc(() => {
+                    if (index == 6) {
+                        let downNode = this.item.shift()
+                        downNode.y = 1290 + 432
+                        this.item.push(downNode)
+                        resolve()
+                    }
+                })))
+            }
+        })
+    }
+
+    async up() {
+        while(true) {
+            await this.moveDown()  
+        }  
     }
 
     start () {
-
+        this.up()
     }
 
-    // update (dt) {}
+    // update (dt) {
+
+    }
 }
